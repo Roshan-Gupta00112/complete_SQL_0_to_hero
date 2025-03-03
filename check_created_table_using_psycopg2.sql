31bf3b5f3adc22aa7b1ea4b8f46f0c99e54c969f
Roshan Kumar <roshangupta00112@gmail.com>
2025-03-03 19:17:07 +0530

    Add files via upload

---
+++ check_created_table_using_psycopg2.sql
@@ -0,0 +1,21 @@
+SELECT rolname, rolpassword FROM pg_authid
+
+
+SELECT current_database();
+
+
+SELECT * FROM information_schema.tables 
+
+
+SELECT * FROM information_schema.tables 
+WHERE table_name = 'psycopg2_table_creation';
+
+
+
+
+select * from students
+
+
+
+
+select * from psycopg2_table_creation
