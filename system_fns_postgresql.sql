cf321a4df88373324cc1c674639051c8021eb0e8
Roshan Kumar <roshangupta00112@gmail.com>
2025-03-13 00:28:39 +0530

    Add files via upload

---
+++ system_fns_postgresql.sql
@@ -0,0 +1,23 @@
+select * from deduplicated;
+
+
+
+
+select current_user;
+-- "postgres"
+
+
+
+select session_user;
+-- "postgres"
+
+
+
+select current_database();
+-- test
+
+
+
+select version();
+-- "PostgreSQL 15.8 (Postgres.app) on aarch64-apple-darwin21.6.0, 
+-- compiled by Apple clang version 14.0.0 (clang-1400.0.29.102), 64-bit"
