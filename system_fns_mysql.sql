53916ef3d55196efe550d5a52e42fbc2583cf7fe
Roshan Kumar <roshangupta00112@gmail.com>
2025-03-13 00:29:14 +0530

    Add files via upload

---
+++ system_fns_mysql.sql
@@ -0,0 +1,24 @@
+select * from deduplicated;
+
+
+
+select user();
+-- root@localhost
+
+
+select current_user();
+-- root@localhost
+
+
+
+select session_user();
+-- root@localhost
+
+
+
+select database();
+-- test
+
+
+select version();
+-- 8.3.0
